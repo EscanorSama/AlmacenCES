0ffb946e44df018e3bd1c8b9f6c7fd60f875ce6b
Angel Albendea <angelalbendea.ces@gmail.com>
2022-03-25 12:09:50 +0100

    Angel: Relaciones e id de las clases relacionadas con Material.java

--- almacen/src/main/java/com/ces/almacen/entities/Material.java
+++ almacen/src/main/java/com/ces/almacen/entities/Material.java
@@ -1,9 +1,39 @@
 package com.ces.almacen.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import net.bytebuddy.implementation.bind.annotation.IgnoreForBinding;
+import org.hibernate.annotations.ManyToAny;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 @Entity
 @Table(name = "material")
 public class Material {
+
+    @Id
+    @GeneratedValue
+    @Column
+    private long id;
+
+    @ManyToMany(mappedBy = "material")
+    @JsonIgnoreProperties({"materiales"})
+    private ArrayList<Categoria> categorias;
+
+    @OneToMany(mappedBy = "material")
+    @JsonManagedReference
+    private Pedido pedido;
+
+
+
 }
