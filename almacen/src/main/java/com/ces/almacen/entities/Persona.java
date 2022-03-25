a1520aa5407fd61085c44e1fe654d84722a4d144
Mateo <mateomora002@gmail.com>
2022-03-25 15:09:44 +0100

    Mateo: Entidades Persona, Alumno, Profesor

--- almacen/src/main/java/com/ces/almacen/entities/Persona.java
+++ almacen/src/main/java/com/ces/almacen/entities/Persona.java
@@ -1,9 +1,39 @@
 package com.ces.almacen.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 @Entity
 @Table(name = "persona")
 public class Persona {
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private long id;
+    @Column(name = "mail")
+    private String mail;
+    @Column(name="nombre")
+    private String nombre;
+    @Column(name = "apellido")
+    private String apellido;
+    @Column(name = "dni")
+    private String dni;
+
+    @OneToOne(mappedBy = "persona")
+    @JsonManagedReference
+    Alumno alumno;
+
+    @OneToOne(mappedBy = "persona")
+    @JsonManagedReference
+    Profesor profesor;
+
 }
--- almacen/src/main/java/com/ces/almacen/entities/Profesor.java
+++ almacen/src/main/java/com/ces/almacen/entities/Profesor.java
@@ -1,10 +1,35 @@
 package com.ces.almacen.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 @Entity
 @Table(name = "profesor")
-public class Profesor
-{
+public class Profesor {
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private long id;
+    @Column(name = "idSolicitud")
+    private long idSolicitud;
+    @Column(name = "idPersona")
+    private long idPersona;
+    @Column(name = "numSs")
+    private int numSs;
+    @Column(name = "salario")
+    private double salario;
+
+    @OneToOne
+    @JsonBackReference
+    Persona persona;
+
 }
